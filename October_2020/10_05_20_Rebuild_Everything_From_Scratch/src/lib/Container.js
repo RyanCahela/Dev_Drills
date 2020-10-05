42202de1402c94625c1149d117f86da1a3f4101b
Ryan Cahela <ryancahela@gmail.com>
2020-10-05 10:23:19 -0700

    added base files, implemented Container.js

---
+++ October_2020/10_05_20_Rebuild_Everything_From_Scratch/src/lib/Container.js
@@ -0,0 +1,33 @@
+class Container {
+  constructor() {
+    this.position = { x: 0, y: 0 };
+    this.nodes = [];
+  }
+
+  add(node) {
+    this.nodes.push(node);
+    return node;
+  }
+
+  remove(node) {
+    this.nodes = this.nodes.filter((n) => n !== node);
+    return node;
+  }
+
+  map(callback) {
+    this.nodes = this.nodes.map(callback);
+  }
+
+  update(deltaTime, currentTime) {
+    this.nodes = this.nodes.filter((node) => {
+      if (node.update) {
+        node.update(deltaTime, currentTime);
+      }
+
+      //if node isDead remove from scene graph
+      return node.isDead ? false : true;
+    });
+  }
+}
+
+export default Container;
